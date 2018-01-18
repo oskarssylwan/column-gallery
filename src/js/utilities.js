cb41cc9e8e4865574b3634d1de51b9c7a6c90aca
Oskars Sylwan <oskars.sylwan@gmail.com>
2018-01-18 14:52:56 +0100

    created array splitting function

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -8,3 +8,28 @@ export const fetch = (url, callback) => {
   script.src = url + '&jsoncallback=jsoncallback';
   gallery.appendChild(script);
 }
+
+export const chunkify = (array, nChunks) => {
+  const length = Math.floor(array.length / nChunks);
+  let arrayCopy = array.slice(0);
+  let output = [];
+
+  if (nChunks === 1) return arrayCopy;
+  else {
+    while ( nChunks > 0 ) {
+      output.push(arrayCopy.splice(0, length));
+      nChunks -= 1;
+    }
+
+    // Handle trailing items
+    if (arrayCopy.length) {
+      let index = 0;
+      while (arrayCopy.length) {
+        output[index].push(arrayCopy.shift(0, 1));
+        index += 1;
+      }
+    }
+
+    return output;
+  }
+}
